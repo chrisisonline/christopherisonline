f35fe760914a5f145f18a44e198447cd9a8544b9
Chris Jiang <Christopherjiang@outlook.com>
2021-12-19 20:10:34 -0500

    Updates

---
+++ precache-manifest.62871fc2cb057c85ef91e3e8d374e6a1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd542b1a57ae68044cadbbd33222a47",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "50c9d8e293ad93fa9ae3",
+    "url": "/portfolio/static/css/main.990c16a7.chunk.css"
+  },
+  {
+    "revision": "c0c4a866715301673195",
+    "url": "/portfolio/static/js/2.56a6d1ab.chunk.js"
+  },
+  {
+    "revision": "50c9d8e293ad93fa9ae3",
+    "url": "/portfolio/static/js/main.f413c89f.chunk.js"
+  },
+  {
+    "revision": "add6ff81d93680170b15",
+    "url": "/portfolio/static/js/runtime-main.2601f1e9.js"
+  },
+  {
+    "revision": "b8cc00bed2b575195805f10eb12f44aa",
+    "url": "/portfolio/static/media/cjlogo.b8cc00be.png"
+  },
+  {
+    "revision": "eb0409cd0715c4971d8565e741760c82",
+    "url": "/portfolio/static/media/design.eb0409cd.png"
+  },
+  {
+    "revision": "ba5fb79ba1142908c9564887323c7548",
+    "url": "/portfolio/static/media/dessertsapp.ba5fb79b.png"
+  },
+  {
+    "revision": "ee6b38d543bacc10e8a83a004ba14a20",
+    "url": "/portfolio/static/media/easyclipboard.ee6b38d5.png"
+  },
+  {
+    "revision": "3a20ddbf0cd3c31ca9c4c3fe76298c02",
+    "url": "/portfolio/static/media/laptop.3a20ddbf.png"
+  },
+  {
+    "revision": "44b333c7e5b35e735f511123bfc9ff79",
+    "url": "/portfolio/static/media/longboard.44b333c7.png"
+  },
+  {
+    "revision": "766030533da7a26485a0f893d1246a83",
+    "url": "/portfolio/static/media/pokemon.76603053.png"
+  },
+  {
+    "revision": "419239e651d0db8dd7f0bfc6248d57d7",
+    "url": "/portfolio/static/media/resumes.419239e6.png"
+  },
+  {
+    "revision": "c50f4b15519b840e10ac0655c8a53395",
+    "url": "/portfolio/static/media/upverter.c50f4b15.png"
+  },
+  {
+    "revision": "15a9ac6f390122d2c2f08fed1e539160",
+    "url": "/portfolio/static/media/website.15a9ac6f.png"
+  }
+]);
